05aceb47ab3efb1b34757637d3f4e52b751643fb
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-06-17 09:34:00 -0700

    fixed restore and backup scripts

--- mysql/5.7/scripts/app/provision/backup.sh
+++ mysql/5.7/scripts/app/provision/backup.sh
@@ -1,18 +1,20 @@
+  GNU nano 3.2                                                                                           provision/backup.sh                                                                                                     
+
 #!/bin/bash
 config="/srv/.global/custom.yml"
-compose="$PWD/.global/docker-compose.yml"
-
-get_sites() {
-  local value=`cat ${config} | shyaml keys sites 2> /dev/null`
-  echo ${value:-$@}
-}
-
-domains=`get_sites`
 
 db_backups=`cat ${config} | shyaml get-value options.db_backups 2> /dev/null`
 
 if [[ ${db_backups} != "False" ]]; then
-	for domain in ${domains//- /$'\n'}; do
-    mysqldump -u root -e "${domain}" > "/srv/databases/${domain}.sql"
-	done
+    mysql --user="root" -e 'show databases' | \
+    grep -v -F "information_schema" | \
+    grep -v -F "performance_schema" | \
+    grep -v -F "mysql" | \
+    grep -v -F "test" | \
+    grep -v -F "Database" | \
+    grep -v -F "sys" | \
+    while read dbname;
+    do
+      mysqldump -uroot -proot "${dbname}" > "/srv/database/backups/${dbname}.sql";
+    done
 fi
