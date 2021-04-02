a70bf605491d500d28f465b3cce88f14a1ad0186
benlumia007 <benlumia007@users.noreply.github.com>
2021-04-02 00:06:21 -0700

    Update databases.sh

--- mysql/8.0/scripts/app/provision/databases.sh
+++ mysql/8.0/scripts/app/provision/databases.sh
@@ -10,10 +10,24 @@ get_sites() {
 }
 
 for domain in `get_sites`; do
-    mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${domain};"
-    mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'wordpress';"
-    mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'wordpress'@'%' WITH GRANT OPTION;"
-    mysql -u root -e "FLUSH PRIVILEGES;"
+    get_site_cms_type() {
+        local value=`cat ${config} | shyaml get-value sites.${domain}.custom.cms_type 2> /dev/null`
+        echo ${value:-$@}
+    }
+
+    type=`get_site_cms_type`
+
+    if [[ "ClassicPress" == ${type} ]]; then
+        mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${domain};"
+        mysql -u root -e "CREATE USER IF NOT EXISTS 'classicpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'classicpress';"
+        mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'classicpress'@'%' WITH GRANT OPTION;"
+        mysql -u root -e "FLUSH PRIVILEGES;"
+    else
+        mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${domain};"
+        mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'wordpress';"
+        mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'wordpress'@'%' WITH GRANT OPTION;"
+        mysql -u root -e "FLUSH PRIVILEGES;"
+    fi
 done
 
 if [[ ${db_restores} != "False" ]]; then
